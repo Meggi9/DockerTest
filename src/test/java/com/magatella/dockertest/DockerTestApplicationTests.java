0195ff68d77b8cd9fdb35def2663a81eeb5b842d
Magomed Muradkhanov <magamuradkhanov@gmail.com>
2022-06-26 22:48:18 +0300

    Initial commit

---
+++ src/test/java/com/magatella/dockertest/DockerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magatella.dockertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
